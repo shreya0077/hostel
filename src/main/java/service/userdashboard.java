ba036d44b1dc499650a56ddadaa8cc2cebde1084
shreya0077 <shreyachandne935@gmail.com>
2023-05-18 21:33:13 +0530

    project setup

---
+++ src/main/java/service/userdashboard.java
@@ -0,0 +1,16 @@
+package service;
+
+public interface userdashboard {
+
+    public void changepassword();	
+    
+    public void updatePhone();
+    
+    public void viewRoom();
+    
+    public void viewFee();
+	
+	
+	
+	
+}
---
+++ src/main/java/serviceImpl/userdashboardImpl.java
@@ -0,0 +1,34 @@
+package serviceImpl;
+
+import service.userdashboard;
+
+public class userdashboardImpl implements userdashboard{
+
+	@Override
+	public void changepassword() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updatePhone() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void viewRoom() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void viewFee() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
+
+}
